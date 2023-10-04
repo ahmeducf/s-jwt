59badc102e652001803958c094a471567365092c
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-10-04 10:47:56 +0300

    test: Add jest configuration
    - Add typescript configuration for ts-jest.
    
    - Add moduleNameMapper to map the the *.js files to *.ts files.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,18 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        // ts-jest configuration goes here
+        tsconfig: './configs/tsconfig.base.json',
+      },
+    ],
+  },
+
+  moduleNameMapper: {
+    './generate.js': './generate.ts',
+    './verify.js': './verify.ts',
+  },
 };
