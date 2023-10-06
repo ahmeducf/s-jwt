8d850b45a322f2b0dc4b6f0e86e7ace06bc5ac8a
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-10-06 14:59:26 +0300

    test: Add unit tests for createHeaderBase64Url function

---
+++ tests/create_header_base64_url.test.ts
@@ -0,0 +1,24 @@
+import { createHeaderBase64Url } from '../src/helpers';
+
+describe('createHeaderBase64Url', () => {
+  it('should create a base64url encoded header with default algorithm "HS256"', () => {
+    const headerBase64Url = createHeaderBase64Url();
+    expect(headerBase64Url).toEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+  });
+
+  it('should create a base64url encoded header', () => {
+    const headerBase64Url = createHeaderBase64Url('HS256');
+    expect(headerBase64Url).toEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+  });
+
+  it('should create a base64url encoded header', () => {
+    const headerBase64Url = createHeaderBase64Url('RS256');
+    expect(headerBase64Url).toEqual('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9');
+  });
+
+  it('should throw an error if algorithm is not supported', () => {
+    expect(() => createHeaderBase64Url('HS123' as never)).toThrowError(
+      '"algorithm" must be one of the supported algorithms',
+    );
+  });
+});
