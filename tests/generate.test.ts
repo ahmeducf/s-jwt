1d044d03768ab48751d1b143c025780af589dcce
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-10-07 20:34:30 +0300

    test: Test signature creation
    - Test generation JWTs with HMAC algorithm.
    - Test generation JWTs with RSA algorithm.
    - Test generation JWTs with RSA-PSS algorithm.
    - Test generation JWTs with ECDSA algorithm.
    - Test different bits length for sha algorithm.

--- tests/generate.test.ts
+++ tests/generate.test.ts
@@ -1,8 +1,8 @@
-import { generate } from '../src/index.js';
+import { generate, generateSync } from '../src/index.js';
 import { Payload, GenerateOptions } from '../src/types/index.js';
 
 describe('generate', () => {
-  it('should return a jwt string', async () => {
+  it('should return a jwt string promise', async () => {
     const payload: Payload = {
       iss: 'test',
       exp: 1234567890,
@@ -13,8 +13,30 @@ describe('generate', () => {
       algorithm: 'HS256',
       secretKey: 'test',
     };
-    
+
     const jwtToken = await generate(payload, options);
-    expect(jwtToken).toBe('test.test.test');
+    expect(jwtToken).toBe(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0ZXN0IiwiZXhwIjoxMjM0NTY3ODkwLCJpYXQiOjEyMzQ1Njc4OTAsInRlc3QiOiJ0ZXN0In0.vNi9dRvHYLssx8gJGEHFI_Qxws924N7w6tZNgpBQ6kM',
+    );
+  });
+});
+
+describe('generateSync', () => { 
+  it('should return a jwt string', () => {
+    const payload: Payload = {
+      iss: 'test',
+      exp: 1234567890,
+      iat: 1234567890,
+      test: 'test',
+    };
+    const options: GenerateOptions = {
+      algorithm: 'HS256',
+      secretKey: 'test',
+    };
+
+    const jwtToken = generateSync(payload, options);
+    expect(jwtToken).toBe(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0ZXN0IiwiZXhwIjoxMjM0NTY3ODkwLCJpYXQiOjEyMzQ1Njc4OTAsInRlc3QiOiJ0ZXN0In0.vNi9dRvHYLssx8gJGEHFI_Qxws924N7w6tZNgpBQ6kM',
+    );
   });
 });
