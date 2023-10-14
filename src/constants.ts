d91afb9be5fa7f79b146064b4de7a1c6efa7c043
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-10-14 22:08:19 +0300

    chore: Implement decodeToken to decode jwt string

--- src/constants.ts
+++ src/constants.ts
@@ -26,8 +26,26 @@ export const ECDSA_PRIVATE_KEY_INVALID: string = 'ECDSA_PRIVATE_KEY_INVALID';
 export const ECDSA_PRIVATE_KEY_INVALID_ERROR_MSG: string =
   'Invalid ECDSA private key: The provided private key is not supported.';
 
+export const JWT_TOKEN_HEADER_INVALID = 'JWT_TOKEN_HEADER_INVALID';
+export const JWT_TOKEN_HEADER_INVALID_ERROR_MSG =
+  'Invalid JWT token header: The header is not a valid JSON object encoded in base64url format.';
+
+export const JWT_TOKEN_PAYLOAD_INVALID = 'JWT_TOKEN_PAYLOAD_INVALID';
+export const JWT_TOKEN_PAYLOAD_INVALID_ERROR_MSG =
+  'Invalid JWT token payload: The payload is not a valid JSON object encoded in base64url format.';
+
+export const JWT_SIGNATURE_INVALID = 'JWT_SIGNATURE_INVALID';
+export const JWT_SIGNATURE_INVALID_ERROR_MSG =
+  'Invalid JWT signature: The signature is not a valid base64url string.';
+
 export default {
   ALGORITHMS,
   RSA_PRIVATE_KEY_INVALID,
   RSA_PRIVATE_KEY_INVALID_ERROR_MSG,
+  ECDSA_PRIVATE_KEY_INVALID,
+  ECDSA_PRIVATE_KEY_INVALID_ERROR_MSG,
+  JWT_TOKEN_HEADER_INVALID,
+  JWT_TOKEN_HEADER_INVALID_ERROR_MSG,
+  JWT_TOKEN_PAYLOAD_INVALID,
+  JWT_TOKEN_PAYLOAD_INVALID_ERROR_MSG,
 };
